6ed04af4f47a45408fde5cb180102a72bd53ff9e
donguk <ldu2175@gmail.com>
2020-04-04 03:05:01 +0900

    스케줄러 조기 종료

---
+++ tbb_project/main.cpp
@@ -0,0 +1,15 @@
+#include <tbb/task_scheduler_init.h>
+using namespace std;
+using namespace tbb;
+
+int main(int argc, char* argv[]) {
+	int nthread = strtol(argv[0], 0, 0);
+	task_scheduler_init init(task_scheduler_init::deferred);
+	if (nthread >= 1)
+		init.initialize(nthread);
+
+	if (nthread >= 1)
+		init.terminate();
+
+	return 0;
+}
